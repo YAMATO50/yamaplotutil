d3f9bc7fa63bdc97444a38afad4e11035d9f0843
YAMATO50 <noah.kuemmerle@gmail.com>
2023-10-22 13:24:49 +0200

    added tests for make_fitted_polynom_values

---
+++ test_functions.py
@@ -0,0 +1,38 @@
+import functions as f
+import numpy as np
+
+def test_make_fittet_polynom_values_int():
+    x = np.linspace(-10, 10, 20, dtype=int)
+    a = 1
+    b = 4
+    c = -2
+    d = 5
+    e = 9
+    coefficients = [a, b, c, d, e]
+    y = f.make_fitted_polynom_values(x, coefficients)
+    for i in range(len(x)):
+        assert y[i] == a*x[i]**4+b*x[i]**3+c*x[i]**2+d*x[i]+e
+
+def test_make_fittet_polynom_values_float():
+    x = np.linspace(-10, 10, 30, dtype=float)
+    a = 1.3
+    b = 4.5
+    c = -2.1
+    d = 5.9
+    e = 0.32
+    coefficients = [a, b, c, d, e]
+    y = f.make_fitted_polynom_values(x, coefficients)
+    for i in range(len(x)):
+        assert y[i] == a*x[i]**4+b*x[i]**3+c*x[i]**2+d*x[i]+e
+
+def test_make_fittet_polynom_values_complex():
+    x = np.linspace(-5-3j, 5+3j, 20, dtype=complex)
+    a = 1.4+0.42j
+    b = 4.0+0j
+    c = -2.2+1.2j
+    d = 0.6-4j
+    e = 0+3j
+    coefficients = [a, b, c, d, e]
+    y = f.make_fitted_polynom_values(x, coefficients)
+    for i in range(len(x)):
+        assert y[i] == a*x[i]**4+b*x[i]**3+c*x[i]**2+d*x[i]+e
