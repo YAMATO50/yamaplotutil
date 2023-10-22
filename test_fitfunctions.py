e5e5b73822f294fc2b96097099154b8564ba4e59
YAMATO50 <noah.kuemmerle@gmail.com>
2023-10-22 12:13:07 +0200

    added tests for fit_cube

--- test_fitfunctions.py
+++ test_fitfunctions.py
@@ -60,3 +60,30 @@ def test_fit_quad_complex():
         c = randComplex(__globalMin, __globalMax)
         x = randComplex(__globalMin, __globalMax)
         assert ff.fit_quad(x, a, b, c) == a*x**2+b*x+c
+
+def test_fit_cube_int():
+    for _ in range(__testRuns):
+        a = randInt(__globalMin, __globalMax)
+        b = randInt(__globalMin, __globalMax)
+        c = randInt(__globalMin, __globalMax)
+        d = randInt(__globalMin, __globalMax)
+        x = randInt(__globalMin, __globalMax)
+        assert ff.fit_cube(x, a, b, c, d) == a*x**3+b*x**2+c*x+d
+
+def test_fit_cube_float():
+    for _ in range(__testRuns):
+        a = randFloat(__globalMin, __globalMax)
+        b = randFloat(__globalMin, __globalMax)
+        c = randFloat(__globalMin, __globalMax)
+        d = randFloat(__globalMin, __globalMax)
+        x = randFloat(__globalMin, __globalMax)
+        assert ff.fit_cube(x, a, b, c, d) == a*x**3+b*x**2+c*x+d
+
+def test_fit_cube_complex():
+    for _ in range(__testRuns):
+        a = randComplex(__globalMin, __globalMax)
+        b = randComplex(__globalMin, __globalMax)
+        c = randComplex(__globalMin, __globalMax)
+        d = randComplex(__globalMin, __globalMax)
+        x = randComplex(__globalMin, __globalMax)
+        assert ff.fit_cube(x, a, b, c, d) == a*x**3+b*x**2+c*x+d
