eedadef497f34d9ef31371868ff8f82d8a74a7e6
YAMATO50 <noah.kuemmerle@gmail.com>
2023-10-22 13:58:37 +0200

    added tests for make_fitted_cos_values

--- test_functions.py
+++ test_functions.py
@@ -67,3 +67,33 @@ def test_make_fitted_sin_values_complex():
     y = f.make_fitted_sin_values(x, a, b, c, d)
     for i in range(len(x)):
         assert y[i] == a*math.sin(b*x[i]+c)+d
+
+def test_make_fitted_cos_values_int():
+    x = np.linspace(-10, 10, 20, dtype=int)
+    a = 1
+    b = 4
+    c = -2
+    d = 5
+    y = f.make_fitted_cos_values(x, a, b, c, d)
+    for i in range(len(x)):
+        assert y[i] == a*math.cos(b*x[i]+c)+d
+
+def test_make_fitted_cos_values_float():
+    x = np.linspace(-10, 10, 30, dtype=float)
+    a = 1.3
+    b = 4.5
+    c = -2.1
+    d = 0.9
+    y = f.make_fitted_cos_values(x, a, b, c, d)
+    for i in range(len(x)):
+        assert y[i] == a*math.cos(b*x[i]+c)+d
+
+def test_make_fitted_cos_values_complex():
+    x = np.linspace(-5, 5, 20, dtype=float)
+    a = 1.4+0.42j
+    b = 4.0
+    c = -2.2
+    d = 0.6-4j
+    y = f.make_fitted_cos_values(x, a, b, c, d)
+    for i in range(len(x)):
+        assert y[i] == a*math.cos(b*x[i]+c)+d
